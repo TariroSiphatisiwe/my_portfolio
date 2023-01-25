19a518cc58d30d6d8f4406ed0a9f14ce2e7a81b2
TariroSiphatisiwe <stariro419@gmail.com>
2023-01-25 13:06:21 +0200

    commiting changes

---
+++ src/components/Navigation.js
@@ -0,0 +1,17 @@
+import React from 'react'
+import flower from './pngs/flower.png'
+import './nav_styles.css'
+import {AiOutlineMenu} from "react-icons/ai"
+
+const Navigation = () => {
+    return (
+        <nav className='nav_bar'>
+            <h2 className='logo'>Tari</h2>
+           <AiOutlineMenu className='svg_outline'/>
+            <img src={flower} alt='flower_canva'/>
+        </nav>
+
+    )
+}
+
+export default Navigation
