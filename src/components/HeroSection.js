49df0858abdcb4323dc68936c7c66ab1207ed78e
TariroSiphatisiwe <stariro419@gmail.com>
2023-01-28 22:03:34 +0200

    added hero section

---
+++ src/components/HeroSection.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import './HeroSec.css'
+
+const HeroSection=()=>{
+
+    return(
+        <div className='hero-sec-div'>
+        <div className='intro-sec-div'><p className='intro-sec'>Welcome to the play ground of Tariro Shumbayaonda </p></div>
+        <p className='intro-sec2'> Heyyyy there, I am Tariro Shumbayaonda. A full-stack Software engineer.This is my corner of the internet. Welcome! </p>
+        
+        </div>
+    )
+}
+
+export default HeroSection
