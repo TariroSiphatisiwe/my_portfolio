2d446bfc748132c3a5594ac4353d6b33331956ce
TariroSiphatisiwe <stariro419@gmail.com>
2023-02-01 12:57:11 +0200

    changes to display

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -1,13 +1,17 @@
 import React from 'react'
 import './HeroSec.css'
+import circle from './circle.svg'
 
-const HeroSection=()=>{
+const HeroSection = () => {
 
-    return(
+    return (
+
         <div className='hero-sec-div'>
-        <div className='intro-sec-div'><p className='intro-sec'>Welcome to the play ground of Tariro Shumbayaonda </p></div>
-        <p className='intro-sec2'> Heyyyy there, I am Tariro Shumbayaonda. A full-stack Software engineer.This is my corner of the internet. Welcome! </p>
-        
+
+            <img src={circle} alt='circle png' className='png circle-png' />
+            <div className='intro-sec-div'><p className='intro-sec'>Welcome to the play ground of Tariro Shumbayaonda </p></div>
+           <p className='intro-sec2'> Heyyyy there, I am Tariro Shumbayaonda. A full-stack Software engineer.This is my corner of the internet. Welcome! </p>
+
         </div>
     )
 }
