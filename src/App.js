f7f80a2b52316501e42496dfa7f260bd3c30f487
TariroSiphatisiwe <stariro419@gmail.com>
2023-02-03 14:17:10 +0200

    Added Rough Notation

--- src/App.js
+++ src/App.js
@@ -1,12 +1,27 @@
 import './App.css'
 import Navigation from './components/Navigation'
 import HeroSection from './components/HeroSection'
+import CustomCursor from 'custom-cursor-react';
+import 'custom-cursor-react/dist/index.css';
+import './components/HeroSec.css'
 
 const App = ()=> {
   return (
-    <div className="App">
+    <div>
       <Navigation/>
       <HeroSection/>
+      <CustomCursor
+      targets={['.circle-png','.intro-sec2','.arrow1-png',]}
+      customClass='custom-cursor'
+      dimensions={50}
+      fill='#cfdbaf'
+      smoothness={{
+        movement: 0.2,
+        scale: 0.1,
+        opacity: 0.2,
+      }}
+      targetOpacity={0.5}
+    />
     </div>
   )
 }
--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -3,21 +3,29 @@ import './HeroSec.css'
 import circle from './circle.svg'
 import arrow1 from './pngs/arrow1.svg'
 import arrow2 from './pngs/arrow2.svg'
+import { RoughNotation, RoughNotationGroup } from "react-rough-notation";
+
+
 const HeroSection = () => {
-    let brac1 = "{"
-    let brac2 = "}"
+
     return (
 
         <div className='hero-sec-div'>
-
+            <RoughNotationGroup show={true}>
             <img src={circle} alt='circle png' className='png circle-png' />
-            <div className='intro-sec-div'><p className='intro-sec'>Welcome to the play ground of Tariro Shumbayaonda </p></div>
-            <p className='intro-sec2'> Heyyyy there, I am Tariro Shumbayaonda. A full-stack Software engineer.This is my corner of the internet. Welcome! </p>
-            <img src={arrow1} alt="arrow png" className="png arrows arrow1-png"/>
-            <img src={arrow2} alt="arrow png" className="png arrows arrow2-png"/>
-            <p className='brac brac1'>{brac1}</p>
-            <p className='brac brac2'>{brac2}</p>
+            <div className='intro-sec-div'><p className='intro-sec'>  <RoughNotation className='rn' type="highlight"  animate={true} color="#f542bf" order="2">Welcome</RoughNotation> to the play ground of Tariro Shumbayaonda </p></div>
+            <p className='intro-sec2'>
+                <RoughNotation type="box" color="#b39dc7" order="1" padding={2}>
+                    Hey there! I'm Tari, A full-stack Software Developer.And this is my corner of the internet. 
+                </RoughNotation>
+            </p>
+            <img src={arrow1} alt="arrow png" className="png arrows arrow1-png" />
+            <img src={arrow2} alt="arrow png" className="png arrows arrow2-png" />
+            </RoughNotationGroup>
         </div>
+
+
+
     )
 }
 
